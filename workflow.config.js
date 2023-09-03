0a5de470d862a9f91333b190900de3b1b0b31b20
yuanzhang <945947732@qq.com>
2023-09-03 17:30:17 +0800

    fix: webhook 环境变量

--- workflow.config.js
+++ workflow.config.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 /**
  * @typedef {import("./index.d.ts").WorkFlowConfig} WorkFlowConfig
  */
@@ -30,12 +31,9 @@ const config = {
     allowBranchs: ['master', 'dev', 'test'],
   },
   notify: {
-    getWebhookUrl: () => 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=e8cc0952-11ac-4c18-96c3-4b6821836fc8',
-    getContent: ({
-      username,
-      tagName,
-    }) => `前端项目正式环境更新，测试环境即将被覆盖，请通知相关测试人员并重新发布测试环境。\n
-      >项目:<font color="comment">综合管理平台</font>
+    getWebhookUrl: () => process.env.WEBHOOK_URL,
+    getContent: ({ username, tagName }) => `前端项目正式环境更新。\n
+      >项目:<font color="comment">项目名字</font>
       >执行人:<font color="comment">${username || '未知'}</font>
       >TAG:<font color="comment">${tagName}</font>`,
     allowBranchs: ['master'],
